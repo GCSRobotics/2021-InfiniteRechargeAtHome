f29f62c6f8a3d66e18427bf61e493b0d581202eb
GCS Programmer <robotics@geneseechristianschool.com>
2021-03-02 18:56:09 -0500

    Shoot Power Cell Command

--- src/main/java/frc/robot/commands/commandgroup/ShootPowerCell.java
+++ src/main/java/frc/robot/commands/commandgroup/ShootPowerCell.java
@@ -1,26 +1,49 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 package frc.robot.commands.commandgroup;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.indexSub.RunIndex;
-import frc.robot.commands.shooterSub.RunShooter;
-import frc.robot.subsystems.IndexSub;
-import frc.robot.subsystems.ShooterSub;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.*;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class ShootPowerCell extends SequentialCommandGroup {
-  /** Creates a new ShootPowerCell. */
-  public ShootPowerCell(ShooterSub shooter, IndexSub indexer) {
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    addCommands(
-      new RunShooter(shooter).withTimeout(.7),
-      new RunIndex(indexer).withTimeout(3)
-    );
+public class ShootPowerCell extends CommandBase {
+  private IndexSub indexSub;
+  private ShooterSub shooterSub;
+
+  public ShootPowerCell(ShooterSub shooter, IndexSub index) {
+    shooterSub = shooter;
+    indexSub = index;
+    addRequirements(indexSub);
+    addRequirements(shooterSub);
+  }
+ 
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    shooterSub.runShooter();
+    indexSub.indexBall();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    shooterSub.stopShooter();
+    indexSub.stopIndex();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
   }
 }
