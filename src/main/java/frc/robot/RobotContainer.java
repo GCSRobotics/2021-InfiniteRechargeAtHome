fd3ee2770f65dafafa92b0322183b7c36e42e104
GCS-Robotics-Driver <robotics@geneseechristianschool.com>
2021-09-14 18:04:10 -0400

    added usb camera server

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -30,6 +31,7 @@ public class RobotContainer {
     private static OI oi = new OI();
 
     public RobotContainer() {
+        CameraServer.getInstance().startAutomaticCapture();
         
     }
 
